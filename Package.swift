de3078ca0d634cce9151adb29ce5cdb7e2a5e097
MIchael Mayerhofer <31078926+eX-innovation@users.noreply.github.com>
2020-04-20 10:10:38 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,7 +1,8 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-    name: "SwiftCron",
-    targets: [],
-    dependencies:[]
+    name: "SwiftCron"
 )
